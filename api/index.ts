752b6abee2f918b6d25adc649a65144decb6332f
dfloresdev <hello@dflores.dev>
2020-06-30 03:09:49 -0500

    #33 [DOCUMENTATION] add swagger

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import travels from './components/travels/network';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+const swaggerDoc = require('./swagger.json');
 app.use('/api/travels', travels);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.set('port', process.env.PORT || 3000);
 
